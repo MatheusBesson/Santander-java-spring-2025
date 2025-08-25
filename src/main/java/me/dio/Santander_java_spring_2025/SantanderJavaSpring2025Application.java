ba1ddffa596f26b1cb976907456a12867f2d521f
Matheus Besson <matheusbesson.main@gmail.com>
2025-08-25 16:04:11 -0300

    Initial commit

---
+++ src/main/java/me/dio/Santander_java_spring_2025/SantanderJavaSpring2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_java_spring_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderJavaSpring2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderJavaSpring2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/Santander_java_spring_2025/SantanderJavaSpring2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_java_spring_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderJavaSpring2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
